b9349cb0c3deccea231c36673c60693e81b85aa1
paulhoulston <Paul.Houlston@hotmail.co.uk>
2015-07-09 14:54:38 +0100

    Added empty website for issue tracker

---
+++ IssueTracker/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web;
+using System.Web.Http;
+
+namespace IssueTracker
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configure(config =>
+            {
+                config.MapHttpAttributeRoutes();
+                config.Routes.MapHttpRoute("DefaultApi", "{controller}/{id}", new {id = RouteParameter.Optional});
+            });
+        }
+    }
+}
---
+++ IssueTracker/IssuesController.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IssueTracker
+{
+    public class IssuesController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return new HttpResponseMessage(HttpStatusCode.NoContent);
+        }
+    }
+}
