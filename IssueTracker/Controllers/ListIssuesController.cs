907dca4117debf1ac818e0af26a3699f04b1696a
paulhoulston <Paul.Houlston@hotmail.co.uk>
2015-07-10 15:54:58 +0100

    Added list issues controller

---
+++ IssueTracker/Controllers/ListIssuesController.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using IssueTracker.Attributes;
+
+namespace IssueTracker.Controllers
+{
+    public class ListIssuesController : ApiController
+    {
+        [GetRoute("Issues")]
+        public async Task<dynamic> Get()
+        {
+            return new dynamic[] { new { item = "item 1" }, new { item = "item 2" }, new { item = "item 3" } };
+        }
+    }
+}
