9c3ef2ce18ec04702e5007576eaf170212eba965
paulhoulston <Paul.Houlston@hotmail.co.uk>
2015-07-10 10:08:20 +0100

    Added create issue controller which currently does not work

--- IssueTracker/IssuesController.cs
+++ IssueTracker/IssuesController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using IssueTracker.Services;
 
 namespace IssueTracker
 {
@@ -10,5 +12,29 @@ namespace IssueTracker
         {
             return new HttpResponseMessage(HttpStatusCode.NoContent);
         }
+    }
+
+    public class CreateIssueController : ApiController
+    {
+        public class Issue
+        {
+            public string CreatedBy { get; set; }    
+        }
+
+        [HttpPost, Route("Issues")]
+        public async Task<HttpResponseMessage> Post(Issue issue)
+        {
+            try
+            {
+                IssueCreationService.ICreateIssues repository = null;
+                new IssueCreationService(repository).CreateIssue(issue.CreatedBy);
+
+                return new HttpResponseMessage(HttpStatusCode.Created);
+            }
+            catch
+            {
+                return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            }
+        }        
     }
 }
