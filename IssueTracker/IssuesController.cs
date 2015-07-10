0c6ce647d6449f85258ec3f54dbde1428e5341f7
paulhoulston <Paul.Houlston@hotmail.co.uk>
2015-07-10 10:17:55 +0100

    Added adapter to write to document db

--- IssueTracker/IssuesController.cs
+++ IssueTracker/IssuesController.cs
@@ -1,8 +1,13 @@
-﻿using System.Net;
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
-using System.Threading.Tasks;
 using System.Web.Http;
 using IssueTracker.Services;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
 
 namespace IssueTracker
 {
@@ -22,11 +27,11 @@ namespace IssueTracker
         }
 
         [HttpPost, Route("Issues")]
-        public async Task<HttpResponseMessage> Post(Issue issue)
+        public HttpResponseMessage Post(Issue issue)
         {
             try
             {
-                IssueCreationService.ICreateIssues repository = null;
+                IssueCreationService.ICreateIssues repository = new IssueRepository();
                 new IssueCreationService(repository).CreateIssue(issue.CreatedBy);
 
                 return new HttpResponseMessage(HttpStatusCode.Created);
@@ -36,5 +41,53 @@ namespace IssueTracker
                 return new HttpResponseMessage(HttpStatusCode.InternalServerError);
             }
         }        
+    }
+
+    public class IssueRepository : IssueCreationService.ICreateIssues
+    {
+        private readonly string _authKeyOrResourceToken = ConfigurationManager.AppSettings["authKey"];
+        private readonly Uri _serviceEndpoint = new Uri(ConfigurationManager.AppSettings["endpoint"]);
+        private const string DatabaseName = "IssueTracker";
+        private const string CollectionId = "Issues";
+
+        public void CreateIssue(IssueCreationService.Issue issue)
+        {
+            using (var client = new DocumentClient(_serviceEndpoint, _authKeyOrResourceToken))
+            {
+                var database = GetDatabase(client)
+                    ?? CreateDatabase(client);
+                
+                var collection = GetDocumentCollection(client, database)
+                    ?? CreateDocumentCollection(client, database);
+
+                var res = client.CreateDocumentAsync(collection.SelfLink, issue).Result;
+            }
+        }
+
+        private static DocumentCollection GetDocumentCollection(DocumentClient client, Database database)
+        {
+            return client.CreateDocumentCollectionQuery(database.SelfLink)
+                .Where(c => c.Id == CollectionId)
+                .AsEnumerable()
+                .SingleOrDefault();
+        }
+
+        private static DocumentCollection CreateDocumentCollection(DocumentClient client, Database database)
+        {
+            var collectionSpec = new DocumentCollection {Id = CollectionId};
+            var requestOptions = new RequestOptions {OfferType = "S1"};
+
+            return client.CreateDocumentCollectionAsync(database.SelfLink, collectionSpec, requestOptions).Result;
+        }
+
+        private static Database GetDatabase(DocumentClient client)
+        {
+            return client.CreateDatabaseQuery().Where(db => db.Id == DatabaseName).AsEnumerable().SingleOrDefault();
+        }
+
+        private static ResourceResponse<Database> CreateDatabase(DocumentClient client)
+        {
+            return client.CreateDatabaseAsync(new Database { Id = DatabaseName }).Result;
+        }
     }
 }
