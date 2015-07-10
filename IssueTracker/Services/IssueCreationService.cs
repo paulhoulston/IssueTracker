044ef9207fbef2353620c71b466bacdd6b0952ba
paulhoulston <Paul.Houlston@hotmail.co.uk>
2015-07-10 09:31:46 +0100

    Added missing file

---
+++ IssueTracker/Services/IssueCreationService.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace IssueTracker.Services
+{
+    public class IssueCreationService
+    {
+        private readonly ICreateIssues _issueCreator;
+
+        public interface ICreateIssues
+        {
+            void CreateIssue(Issue issue);
+        }
+
+        public class Issue
+        {
+            public string CreatedBy { get; set; }
+            public DateTime CreatedTime { get; set; }
+            public int Id { get; set; }
+        }
+
+        public IssueCreationService(ICreateIssues issueCreator)
+        {
+            _issueCreator = issueCreator;
+        }
+
+        public void CreateIssue(string createdBy)
+        {
+            _issueCreator.CreateIssue(new Issue
+            {
+                CreatedBy = createdBy,
+                CreatedTime = DateTime.UtcNow
+            });
+        }
+    }
+}
